7228e254f49048ae35835d4a9c04af3456a2b8fe
Clement.Malet <malet.pro@gmail.com>
2014-06-15 16:04:03 +0200

    Grid init and few event handling

--- Citizen-Me/js/controllers.js
+++ Citizen-Me/js/controllers.js
@@ -1,7 +1,6 @@
 var gameApp = angular.module('gameApp', []);
 
 gameApp.controller('gameAreaCtrl', function ($scope, $http) {
-	var number = 50;
 	
 	// Initialization of the grid from grid.json file
 	$http.get('files/grid.json').
@@ -16,15 +15,34 @@ gameApp.controller('gameAreaCtrl', function ($scope, $http) {
 			}
 	    }).
 	    error(function(data) {
-	    	alert('no');
+	    	// If using the web app locally, some browsers won't allow to $http.get files from different folders
+	    	// Mozilla Firefox should allow it though
+	    	alert('Grid cannot be loaded.');
 	    });
 	
 	$scope.range = function(number) {
 		return new Array(number);
 	};
 	
-	$scope.showCoordinates = function (x, y) {
-		// alert (x + ';' + y);
+	$scope.highlight = function(x, y) {
+		angular.element(document.querySelector('#tile_' + x + '_' + y)).removeClass('tile').addClass('tile_highlighted');
 	};
-
+	
+	$scope.resetCoordinates = function () {
+		$scope.coordinates = "";
+	};
+	
+	$scope.showCoordinates = function (x, y) {
+		$scope.coordinates = x + " ; " + y;
+	};
+	
+	$scope.selectElementToAdd = function (tile) {
+		var url_cursor = "'" + "img/" + tile + ".jpg" + "'";
+		document.body.style.cursor = "url(" + url_cursor + "), auto";
+	};
+	
+	$scope.resetCursor = function () {
+		document.body.style.cursor = 'auto';
+	};
+	
 });
