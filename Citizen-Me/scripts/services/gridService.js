b6dbf73725ea09432cdd4ba1152758534c1db2b7
Clement.Malet <malet.pro@gmail.com>
2014-06-21 14:01:54 +0200

    Forced commit

--- Citizen-Me/scripts/services/gridService.js
+++ Citizen-Me/scripts/services/gridService.js
@@ -23,13 +23,11 @@ gameApp.service ('GridService', function ($http, PriceService, MoneyService) {
 						}
 					}
 				}
-			var grid = angular.element(document.querySelector('#game'));
-			var centerTopGrid = (grid[0].scrollTopMax)/2;
-			var centerLeftGrid = (grid[0].scrollLeftMax)/2;
-			grid[0].scrollTop = centerTopGrid;
-			grid[0].scrollLeft = centerLeftGrid;
-
-
+				var grid = angular.element(document.querySelector('#game'));
+				var centerTopGrid = (grid[0].scrollTopMax)/2;
+				var centerLeftGrid = (grid[0].scrollLeftMax)/2;
+				grid[0].scrollTop = centerTopGrid;
+				grid[0].scrollLeft = centerLeftGrid;
 		    }).
 		    error(function(data) {
 		    	// If using the web app locally, some browsers won't allow to $http.get files from different folders
